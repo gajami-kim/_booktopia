a089346ed65452d5e906ddf36fe1ebbb9e16f4d7
gajami-kim <qansd38@gmail.com>
2024-07-01 16:01:15 +0900

    0701 commit

---
+++ www/src/main/java/com/booktopia/www/domain/PagingVO.java
@@ -0,0 +1,46 @@
+package com.booktopia.www.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PagingVO {
+
+    private int pageNo;
+    private int qty;
+    private String type;
+    private String keyword;
+    private String bCate;
+    private String sortCate;
+
+    public PagingVO(){
+        this.pageNo=1;
+        this.qty=10;
+    }
+
+    public PagingVO(int pageNo, int qty){
+        this.pageNo=pageNo;
+        this.qty=qty;
+    }
+
+    public PagingVO(int pageNo, int qty, String type, String keyword, String bCate,String sortCate){
+        this.pageNo=pageNo;
+        this.qty=qty;
+        this.type=type;
+        this.keyword=keyword;
+        this.bCate=bCate;
+        this.sortCate=sortCate;
+    }
+
+    public int getPageStart(){
+        return (pageNo-1)*qty;
+    }
+
+    public String[] getTypeToArray(){
+        return this.type==null? new String[]{}:this.type.split("");
+    }
+
+}
